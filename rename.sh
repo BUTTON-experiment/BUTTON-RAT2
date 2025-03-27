7fb1e7566589a49e521055c108ceb7dd3b00ee13
LSexton2396 <62307034+LSexton2396@users.noreply.github.com>
2025-03-27 20:11:22 +0000

    Update rename.sh

--- rename.sh
+++ rename.sh
@@ -9,6 +9,15 @@ then
   basestring=${1,,}
   upstring=${basestring^^}
   mixedstring=${basestring^}
+  
+  if [[ "$lower" == "$basestring" ]] && [[ "$upper" == "$upstring" ]] && [[ "$mixed" == "$mixedstring" ]];
+  then
+          echo -e "\n It appears you have already run this script. There is no need to run it again. \n"
+          exit
+  fi
+
+  echo -e "\n Changing $lower, $upper, and $mixed to $basestring, $upstring, and $mixedstring."
+  echo -e "\n DO NOT RUN THIS SCRIPT MORE THAN ONCE! \n"
 
   find . -type f -not -path "./.git/*" -exec sed -i "s/${lower}/${basestring}/g" {} \;
   find . -type f -not -path "./.git/*" -exec sed -i "s/${upper}/${upstring}/g" {} \;
