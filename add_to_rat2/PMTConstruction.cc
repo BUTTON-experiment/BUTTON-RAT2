220a8dcc9eccf0af6573e6db7847cce099738406
LSexton2396 <lewissexton2396@gmail.com>
2025-04-02 20:57:14 +0100

    updated to work with latest rat version

--- add_to_rat2/PMTConstruction.cc
+++ add_to_rat2/PMTConstruction.cc
@@ -5,7 +5,7 @@
 #include <G4Tubs.hh>
 #include <G4VisAttributes.hh>
 #include <RAT/CubicPMTConstruction.hh>
-#include <RAT/EncapsulatedPMTConstruction.hh>
+#include <RAT/CylindricalPMTConstruction.hh>
 #include <RAT/GLG4PMTOpticalModel.hh>
 #include <RAT/LAPPDConstruction.hh>
 #include <RAT/Log.hh>
@@ -13,27 +13,29 @@
 #include <RAT/PMTConstruction.hh>
 #include <RAT/RevolutionPMTConstruction.hh>
 #include <RAT/ToroidalPMTConstruction.hh>
+#include <RAT/EncapsulatedPMTConstruction.hh>
 #include <algorithm>
 
 namespace RAT {
 
-PMTConstruction *PMTConstruction::NewConstruction(DBLinkPtr table,
-                                                  G4LogicalVolume *mother) {
+PMTConstruction *PMTConstruction::NewConstruction(DBLinkPtr table, G4LogicalVolume *mother) {
   std::string construction = table->Get<std::string>("construction");
   if (construction == "toroidal") {
     return new ToroidalPMTConstruction(table, mother);
-  } else if (construction == "encapsulated") {
-    return new EncapsulatedPMTConstruction(table, mother);
   } else if (construction == "revolution") {
     return new RevolutionPMTConstruction(table, mother);
+  } else if (construction == "cylindrical") {
+    return new CylindricalPMTConstruction(table, mother);
   } else if (construction == "cubic") {
     return new CubicPMTConstruction(table, mother);
   } else if (construction == "lappd") {
     return new LAPPDConstruction(table, mother);
+  } else if (construction == "encapsulated") {
+    return new EncapsulatedPMTConstruction(table, mother);
   } else {
     Log::Die("PMT construction \'" + construction + "\' does not exist.");
   }
   return NULL;
 }
 
-} // namespace RAT
+}  // namespace RAT
